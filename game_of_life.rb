99c46166647a8b645ae89d02eb4b5ef2748065bb
Thiago Rocha <kimo@kimo.io>
2013-11-06 12:50:54 -0200

    Refactoring Rule #3: Any live cell with more than three live neighbours dies, as if by overcrowding.

--- game_of_life.rb
+++ game_of_life.rb
@@ -72,6 +72,7 @@ class World
   def rotate!
     apply_rule_1
     apply_rule_2
+    apply_rule_3
   end
 
   def create_cell(cell)
@@ -89,6 +90,12 @@ class World
   def apply_rule_2
     @cells.each do |cell|
       cell.alive = true if live_neighbours_of(cell).count == 2 or live_neighbours_of(cell).count == 3
+    end
+  end
+
+  def apply_rule_3
+    @cells.each do |cell|
+      cell.die! if cell.alive? and live_neighbours_of(cell).count > 3
     end
   end
 end
@@ -160,6 +167,69 @@ describe "The Game of Life" do
       world.rotate!
 
       world.cells.first.alive?.must_equal true
+    end
+  end
+
+  describe "Rule #3: Any live cell with more than three live neighbours dies, as if by overcrowding." do
+    it "a cell with 4 live neighbours will die in the next day" do
+      world = World.new
+
+      # (-1,0) |  (0,0) |  (1,0)
+      # cell_2 | cell_1 | cell_3
+      # -------|--------|-------
+      # cell_4 | cell_5 |
+      # (-1,-1)| (0,-1)
+      cell_1 = Cell.new(0,0)
+      cell_2 = Cell.new(-1,0)
+      cell_3 = Cell.new(1,0)
+      cell_4 = Cell.new(-1,-1)
+      cell_5 = Cell.new(0,-1)
+
+      world.create_cell(cell_1)
+      world.create_cell(cell_2)
+      world.create_cell(cell_3)
+      world.create_cell(cell_4)
+      world.create_cell(cell_5)
+
+      world.rotate!
+
+      world.cells.first.dead?.must_equal true
+    end
+
+    it "a cell surrounded by live neighbours must die in the next day" do
+      world = World.new
+
+      # (-1,1) | (0,1)  | (1,1)
+      # cell_7 | cell_8 | cell_9
+      # -------|--------|-------
+      # (-1,0) |  (0,0) |  (1,0)
+      # cell_2 | cell_1 | cell_3
+      # -------|--------|-------
+      # cell_4 | cell_5 | cell_6
+      # (-1,-1)| (0,-1) | (1,-1)
+      cell_1 = Cell.new(0,0)
+      cell_2 = Cell.new(-1,0)
+      cell_3 = Cell.new(1,0)
+      cell_4 = Cell.new(-1,-1)
+      cell_5 = Cell.new(0,-1)
+      cell_6 = Cell.new(1,-1)
+      cell_7 = Cell.new(-1,1)
+      cell_8 = Cell.new(0,1)
+      cell_9 = Cell.new(1,1)
+
+      world.create_cell(cell_1)
+      world.create_cell(cell_2)
+      world.create_cell(cell_3)
+      world.create_cell(cell_4)
+      world.create_cell(cell_5)
+      world.create_cell(cell_6)
+      world.create_cell(cell_7)
+      world.create_cell(cell_8)
+      world.create_cell(cell_9)
+
+      world.rotate!
+
+      world.cells.first.dead?.must_equal true
     end
   end
 end
