0c3cc3cec5d7206ca3d1fe49276039b3a38e101a
Thiago Rocha <kimo@kimo.io>
2013-11-10 22:59:55 -0200

    Adding the gosu file template

--- game.rb
+++ game.rb
@@ -1,11 +1,24 @@
 require 'gosu'
+require './lib/game_of_life'
 
-class MyWindow < Gosu::Window
-  def initialize
-   super(640, 480, false)
-   self.caption = 'Hello World!'
+class GameOfLifeWindow < Gosu::Window
+  def initialize(width=640, height=480)
+    @width, @height = width, height
+   super(@width, @height, false)
+   self.caption = 'The Game of Life'
+  end
+
+  def update
+  end
+
+  def draw
+
+  end
+
+  def needs_cursor?
+    true
   end
 end
 
-window = MyWindow.new
+window = GameOfLifeWindow.new
 window.show
